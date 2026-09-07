cc42f4603d35e6a174f8ae4a542d107d0e6f82bf
sociedadeguarani <sociedader.guarani@gmail.com>
2026-09-07 17:21:25 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
  "use client";
 
 import { useEffect } from "react";
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+import { supabase } from "@/lib/supabaseClient";
 
 export default function Home() {
   useEffect(() => {
