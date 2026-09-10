22c24b56246c93f06216cca7d911284740af1444
sociedadeguarani <sociedader.guarani@gmail.com>
2026-09-10 09:06:26 -0300

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CalendarDays, LogIn, ShieldCheck, Users } from "lucide-react";
 import { supabase } from "@/lib/supabaseClient";
 
