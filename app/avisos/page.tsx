4fb9dea5dc4e429c7d751c3589c19bcd5af461cf
sociedadeguarani <sociedader.guarani@gmail.com>
2026-09-14 13:22:44 -0300

    Update page.tsx

--- app/avisos/page.tsx
+++ app/avisos/page.tsx
@@ -284,7 +284,6 @@ export default function AvisosPage(){
               </div>
             )}
           </div>}
-      </div>
 
       <div className="grid gap-5 md:grid-cols-2 xl:grid-cols-3">
         {avisos.map(a=>
